678c88687d1f5243001b190945e639590fc95ef7
Damian-Czarnota <yziek@windowslive.com>
2018-11-06 13:42:04 +0100

    Adds router to app

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -5,14 +5,15 @@ import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
 import store from "./store/index";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter><App /></BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
