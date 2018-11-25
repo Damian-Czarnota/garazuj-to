df56d2d0b73865f8c78e93357616c9b91884d7f2
Damian-Czarnota <yziek@windowslive.com>
2018-11-25 13:12:00 +0100

    Adds methods to add/get cars and add/delete avatar

--- web-app/src/API/Me.js
+++ web-app/src/API/Me.js
@@ -17,17 +17,36 @@ export const edit = (data) =>
         headers:headers,
         body:JSON.stringify(data)
     })
-        .then(res => res.json());
+        .then(res => res);
 
 export const change = (file) =>{
     let fd = new FormData();
     fd.append('file', new Blob([(file[0])], {
         type: file[0].type
     }),file[0].name);
-    return fetch(`${URL}/file/upload`,{
+    return fetch(`${URL}/me`,{
         method:'POST',
         headers:fileUploadHeader,
         body:fd
     })
-        .then(res => res.json());
+        .then(res => res);
 };
+
+export const addCar = (car) =>
+    fetch (`${URL}/me/car`,{
+        method:'POST',
+        headers:headers,
+        body:JSON.stringify(car)
+    }).then(res => res);
+
+export const getCars = () =>
+    fetch (`${URL}/me/car`,{
+        method:'GET',
+        headers:headers
+    }).then(res => res.json());
+
+export const deleteAvatar = () =>
+    fetch (`${URL}/me`,{
+        method:'DELETE',
+        headers:headers
+    }).then(res => res);
