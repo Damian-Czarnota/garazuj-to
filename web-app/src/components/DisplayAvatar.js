617f278b13f7bee38047e911dab2ca28383c6988
Damian-Czarnota <yziek@windowslive.com>
2018-11-25 14:39:58 +0100

    Removes useless contructor

--- web-app/src/components/DisplayAvatar.js
+++ web-app/src/components/DisplayAvatar.js
@@ -1,10 +1,6 @@
 import React, {Component, Fragment} from 'react';
 
 export default class DisplayAvatar extends Component{
-    constructor(props){
-        super(props);
-    }
-
     render(){
         let {profileImage,size} = this.props;
         return(
